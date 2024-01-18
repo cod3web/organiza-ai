a906682d557895ba58ece7f2e48017cf3498effd
valdir-amaral <valdir.amaral58@gmail.com>
2024-01-18 16:51:06 -0300

    feat: login page ui

---
+++ src/components/Button.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let onClick = new Function(), 
+            text, 
+            submitButton;
+  
+  
+</script>
+
+{#if submitButton}
+<input type="submit" value={text}>
+{:else}
+<button on:click={onClick}>
+  {text}
+</button>
+{/if}
+
+<style>
+  button, input {
+    background-color: #8153E2;
+    color: white;
+    font-family: 'Manrope';
+    font-weight: 700;
+    text-transform: uppercase;
+    padding: 12px;
+    outline: none;
+    border: none;
+    border-radius: 12px;
+    cursor: pointer;
+    transition: all .2s ease-in-out;
+  }
+  button:hover {
+    opacity: .8;
+    transform: translateY(-2px);
+  }
+</style>
--- src/views/Login.svelte
+++ src/views/Login.svelte
@@ -1,3 +1,14 @@
+<script>
+  import { pbStore } from "../lib/store";
+  import Button from "../components/Button.svelte";
+
+
+  function login(ev) {
+    ev.preventDefault();
+    console.log($pbStore)
+    alert('oi')
+  }
+</script>
 <main>
   <section>
     <div class="left-part">
@@ -5,17 +16,21 @@
     </div>
 
     <div class="right-part">
-      <h1>Lorem Ipsum</h1>
-      <h2>Entrar na sua conta</h2>
-      <p>Lorem ipsum is simply dummy text of the printing.</p>
-      <form>
-        <label for="email">E-mail</label>
-        <input placeholder="Insira seu e-mail" class="input" type="email" name="email" id="email">
-        <label for="password">Senha</label>
-        <input class="input" placeholder="Insira sua senha" type="password" name="password" id="password">
-        <button>Entrar</button>
+      <h1 class="title">Lorem Ipsum</h1>
+      <h2 class="subtitle">Entrar na sua conta</h2>
+      <p class="intro">Lorem ipsum is simply dummy text of the printing.</p>
+      <form on:submit={login}>
+        <div class="form-group">
+          <label for="email">E-mail</label>
+          <input required placeholder="Insira seu e-mail" class="input" type="email" name="email" id="email">
+        </div>
+        <div class="form-group">
+          <label for="password">Senha</label>
+          <input required class="input" placeholder="Insira sua senha" type="password" name="password" id="password">
+        </div>
+        <Button submitButton={true} text="Entrar" />
       </form>
-      <p>&copy; Lorem ipsum 2023</p>
+      <p class="copy">&copy; Codeweb Studios {new Date().getFullYear()}</p>
     </div>
   </section>
 </main>
@@ -23,25 +38,52 @@
 <style>
   section {
     display: flex;
-    background: linear-gradient(to right, rgb(76, 76, 189) 50%, white 50%);
+    background: linear-gradient(to right, #151561 50%, white 50%);
 
   }
   section > * {
     width: 50%;
   }
+  label {
+    font-family: 'Urbanist';
+    font-weight: 500;
+  }
   .left-part {
     height: 100vh;
   }
-
+  label {
+    margin-bottom: 5px;
+  }
   .right-part {
     padding: 24px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding-top: 120px;
+    margin-top: 120px;
   }
   form {
+    width: 100%;
+    max-width: 380px;
     display: flex;
     flex-direction: column;
+    margin-top: 50px;
+    gap: 20px;
+  }
+  input {
+    width: 100%;
+  }
+  .title {
+    text-transform: uppercase;
+    font-size: 16px;
+    margin-bottom: 30px;
+    color: #b468d3;
+  }
+  .subtitle {
+    font-size: 24px;
+    margin-bottom: 15px;
+  }
+  .intro {
+    color: #B8B8B8;
+    font-size: 16px;
   }
 </style>
