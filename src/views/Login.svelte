363e471d59ca4a565fed77240ebe3c0cd3c268ff
valdir-amaral <valdir.amaral58@gmail.com>
2024-01-19 10:26:00 -0300

    feat: add cool style for login page

--- src/views/Login.svelte
+++ src/views/Login.svelte
@@ -21,13 +21,20 @@
 <main>
   <section>
     <div class="left-part">
+      <div class="overlay">
+        <div class="overlay-content">
+          <h1>Seja muito bem-vindo(a)</h1>
+          <p>Transforme sua paixão em negócio. Simplificamos, você cria. Descubra o poder do Proartivo para impulsionar o seu artesanato.</p>
+        </div>
 
+      </div>
+      <video src="/media/login.mp4" autoplay loop playsinline muted></video>
     </div>
 
     <div class="right-part">
       <h1 class="title">Proartivo</h1>
       <h2 class="subtitle">Entrar na sua conta</h2>
-      <p class="intro">Você está a um passo de organizar tudo da sua lojinha!</p>
+      <p class="intro">Você está a um passo de organizar tudo da sua loja!</p>
       <form on:submit={login}>
         <div class="form-group">
           <label for="email">E-mail</label>
@@ -50,6 +57,49 @@
     background: linear-gradient(to right, #151561 50%, white 50%);
 
   }
+  video {
+    object-fit: cover;
+    display: block;
+    position: absolute;
+    height: 100% !important;
+    width: 100% !important;
+    max-width: 100% !important;
+    top: 0;
+    left: 0;
+    background-color: #000;
+  }
+  .left-part .overlay {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    z-index: 200;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background-color: rgba(83, 51, 223, 0.4);
+  }
+  .overlay .overlay-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: -120px;
+  }
+  .overlay h1 {
+    font-size: 48px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .overlay p {
+    text-align: center;
+    font-size: 18px;
+    line-height: 1.5;
+    max-width: 800px;
+  }
   section > * {
     width: 50%;
   }
@@ -59,6 +109,7 @@
   }
   .left-part {
     height: 100vh;
+    position: relative;
   }
   label {
     margin-bottom: 5px;
@@ -68,7 +119,7 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 120px;
+    margin-top: 200px;
   }
   form {
     width: 100%;
